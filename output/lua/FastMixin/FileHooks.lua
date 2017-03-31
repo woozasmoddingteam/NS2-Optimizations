bcad29d92720f16c8ed4b67f5f02392ea4b525ad
Las <http://github.com/Laaas/>
2017-03-31 13:27:02 +0200

    Move a bunch of fast mixin's code from the entry file to the filehooks file.

--- output/lua/FastMixin/FileHooks.lua
+++ output/lua/FastMixin/FileHooks.lua
@@ -1,4 +1,92 @@
+local toString = function(v)
+	local err, str = pcall(ToString, v);
+	if type(v) == "table" then
+		return tostring(v) .. " " .. str;
+	elseif type(v) == "string" then
+		return "\"" .. str .. "\"";
+	else
+		return str;
+	end
+end
+
+debug.callstack = function(level)
+	level = level and level + 1 or 2;
+	local str = "Call stack:";
+	local i = level;
+	local func = debug.getinfo(i);
+	while func do
+		str = str .. ("\n\t#%d: %s:%i%s:%i"):format(i-level+1, func.name, func.currentline, func.source, func.linedefined);
+		local j = 1;
+		local name, value = debug.getlocal(i, j);
+		while name do
+			str = str .. ("\n\t\t%s = %s"):format(name, toString(value));
+			j = j + 1;
+			name, value = debug.getlocal(i, j);
+		end
+		i = i + 1;
+		func = debug.getinfo(i);
+	end
+	return str;
+end
+
+local oldclass = class;
+
+local reg = debug.getregistry();
+local classes = reg.__CLASSES;
+if not classes then
+	classes = {};
+	reg.__CLASSES = classes;
+end
+
+local function GetMixinConstants(self)
+	return self.__mixindata
+end
+
+local function GetMixinConstant(self, constantName)
+	return self.__mixindata[constantName]
+end
+
+Entity.__is_ent = true;
+
+class = function(name)
+	local oldbasesetter = oldclass(name);
+	local cls = _G[name];
+	local meta = getmetatable(cls);
+	meta.name = name;
+	meta.mixintypes = {};
+	meta.mixindata = {};
+	--meta.mixinarguments = {};
+	-- Used to restore the original functions and unapplying the mixins. Integer keys's values are keys that should be cleared.
+	meta.mixinbackup = {};
+	meta.mixins = {};
+	classes[#classes+1] = cls;
+
+	cls.__is_ent = false;
+	cls.GetMixinConstants = GetMixinConstants;
+	cls.GetMixinConstant = GetMixinConstant;
+
+	return function(base)
+		meta.base = base;
+		if base == Entity then
+			cls.__is_ent = true;
+		end
+		oldbasesetter(base);
+		---[=[
+		local backup = getmetatable(base) and getmetatable(base).mixinbackup;
+		if backup then
+			for k, v in pairs(backup) do
+				if type(k) ~= "number" then
+					cls[k] = v;
+				end
+			end
+			for i = 1, #backup do
+				cls[backup[i]] = nil;
+			end
+		end
+	end
+end
+
 ModLoader.SetupFileHook("lua/MixinUtility.lua", "lua/FastMixin/MixinUtility.lua", "replace");
-ModLoader.SetupFileHook("lua/MixinDispatcherBuilder.lua", "", "halt");
+ModLoader.SetupFileHook("lua/MixinDispatcherBuilder.lua", true, "halt");
 ModLoader.SetupFileHook("lua/Mixins/BaseModelMixin.lua", "lua/FastMixin/BaseModelMixin.lua", "post");
 ModLoader.SetupFileHook("lua/ScoringMixin.lua", "lua/FastMixin/ScoringMixin.lua", "post");
