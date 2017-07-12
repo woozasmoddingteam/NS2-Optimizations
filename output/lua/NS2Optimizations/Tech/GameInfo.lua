b3ecee5110473dc5222ec6a8d0c1e0502b02ba5f
Las <las@protonmail.ch>
2017-07-12 23:32:22 +0200

    Fix GameInfo calling function that are not yet declared

--- output/lua/NS2Optimizations/Tech/GameInfo.lua
+++ output/lua/NS2Optimizations/Tech/GameInfo.lua
@@ -1,15 +1,20 @@
 
+TechData_Initial_InWarmUp = false
 local zero = false
 local function check(self)
-	local state = self.state
-	if state == kGameState.WarmUp then
-		if zero == false then
-			ZeroCosts()
-			zero = true
+	if ZeroCosts ~= nil then
+		local state = self.state
+		if state == kGameState.WarmUp then
+			if zero == false then
+				ZeroCosts()
+				zero = true
+			end
+		elseif zero == true then
+			RestoreCosts()
+			zero = false
 		end
-	elseif zero == true then
-		RestoreCosts()
-		zero = false
+	else
+		TechData_Initial_InWarmUp = state == kGameState.WarmUp
 	end
 end
 
