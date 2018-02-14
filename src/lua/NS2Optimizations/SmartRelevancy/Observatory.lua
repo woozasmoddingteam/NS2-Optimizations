2274c4e223eeaafa70cee9a91dd951fd315f8976
Las <las@protonmail.ch>
2018-02-14 16:29:55 +0100

    Update for 321

--- src/lua/NS2Optimizations/SmartRelevancy/Observatory.lua
+++ src/lua/NS2Optimizations/SmartRelevancy/Observatory.lua
@@ -1,104 +1,39 @@
-
-do
-	function Observatory:FindCommandStation()
-		self.nearest_commandstation = GetNearest(self:GetOrigin(), "CommandStation", self:GetTeamNumber(), Lambda [[(...):GetIsBuilt() and (...):GetIsAlive()]]):GetId()
-		return Shared.GetEntity(self.nearest_commandstation)
-	end
-
-	function Observatory:GetCommandStation()
-		return Shared.GetEntity(self.nearest_commandstation) or self:FindCommandStation()
-	end
-
-	function Observatory:GetDistressOrigin()
-		local cc = self:GetCommandStation()
-		return cc and cc:GetModelOrigin()
-	end
+function Observatory:FindCommandStation()
+	self.nearest_commandstation = GetNearest(self:GetOrigin(), "CommandStation", self:GetTeamNumber(), Lambda [[(...):GetIsBuilt() and (...):GetIsAlive()]]):GetId()
+	return Shared.GetEntity(self.nearest_commandstation)
 end
 
---[[
-	Only server
-]]
-if not Server then return end
+function Observatory:GetCommandStation()
+	return Shared.GetEntity(self.nearest_commandstation) or self:FindCommandStation()
+end
 
+function Observatory:GetDistressOrigin()
+	local cc = self:GetCommandStation()
+	return cc and cc:GetModelOrigin()
+end
 
 local kDistressBeaconTime = Observatory.kDistressBeaconTime
-local kDistressBeaconEnd  = kDistressBeaconTime + 10
-local kDistressBeaconRange = Observatory.kDistressBeaconRange
 
-local kIgnorePlayers = kNS2OptiConfig.InfinitePlayerRelevancy
-
-local function makeRelevant(self)
-	self.__old_include_mask = self:GetIncludeRelevancyMask()
-	self:SetIncludeRelevancyMask(0xFFFFFFFF)
-end
-
-local function makeIrrelevant(self)
-	self:SetIncludeRelevancyMask(self.__old_include_mask)
-end
-
-local function prepareBeacon(self, delay, los_time)
-	if not kIgnorePlayers then
-		self:AddTimedCallback(makeRelevant, delay)
-		self:AddTimedCallback(makeIrrelevant, kDistressBeaconEnd)
-	end
-	self.timeLastLOSDirty   = los_time + delay * 10
-	self.timeLastLOSUpdate  = los_time + delay * 10 + 0.5
-	self.__x4841 = los_time
-end
-
-local oldTriggerDistressBeacon = Observatory.TriggerDistressBeacon
+local old = Observatory.TriggerDistressBeacon
 
 function Observatory:TriggerDistressBeacon()
 	self:FindCommandStation()
 	local distressOrigin = self:GetDistressOrigin()
-	local los_time = Shared.GetTime() + kDistressBeaconEnd
 
 	-- May happen at the end of the game?
 	if not distressOrigin or self:GetIsBeaconing() then
 		return false, true
 	end
 
-	local step = kDistressBeaconTime / Server.GetNumPlayers()
-	GetGamerules():GetTeam1():ForEachPlayer(Closure [=[
-		self prepareBeacon step los_time
-		args player
-
-		if player:isa "Marine" then
-			prepareBeacon(player, self.delay, los_time)
-			self.delay = self.delay + step
-		end
-	]=] {prepareBeacon, step, los_time, delay = 0})
-
 	local ips = GetEntities "InfantryPortal"
-	local step = (kDistressBeaconTime - 0.1) / #ips
+	local step = (kDistressBeaconTime * .5) / #ips
 	local delay = 0
 	for i = 1, #ips do
 		ips[i]:AddTimedCallback(ips[i].FinishSpawn, delay)
 		delay = delay + step
 	end
 
-	-- We need to check for LiveMixin, since some entities (e.g. map blips)
-	-- are not supposed to be relevant unconditionally
-	local entities = self:GetCommandStation():GetLocationEntity():GetEntitiesInTrigger()
-	if #entities == 0 then
-		Shared.Message "Found no entities in the location entity!"
-		entities = GetEntitiesWithMixinWithinRange("Live", distressOrigin, 20)
-	end
-	Shared.Message("Found " .. #entities .. " live entities near distress origin")
-	local step = kDistressBeaconTime / #entities
-	local delay = 0
-	for i = 1, #entities do
-		local ent = entities[i]
-		if HasMixin(ent, "Live") and ent.__x4841 ~= los_time then
-			ent:AddTimedCallback(makeRelevant, delay)
-			ent:AddTimedCallback(makeIrrelevant, kDistressBeaconEnd)
-			ent.timeLastLOSDirty   = los_time + delay * 10
-			ent.timeLastLOSUpdate  = los_time + delay * 10 + step * (10 / 2)
-			delay = delay + step
-		end
-	end
-
-	return oldTriggerDistressBeacon(self)
+	return old(self)
 end
 
 function Observatory:PerformDistressBeacon()
@@ -111,7 +46,7 @@ function Observatory:PerformDistressBeacon()
 	end
 
 	local spawnPoints = GetBeaconPointsForTechPoint(self:GetCommandStation().attachedId)
-	
+
 	if not spawnPoints then
 		return
 	end
@@ -120,6 +55,16 @@ function Observatory:PerformDistressBeacon()
 		self toOrigin spawnPoints
 		args player
 		if player:isa "Marine" and (player:GetOrigin() - toOrigin):GetLengthSquared() > (kDistressBeaconRange*1.1)^2 then
+			if Server then
+				local f = player.MarkNearbyDirtyImmediately
+				if f then
+					f(player)
+				end
+				local f = player.OnPreBeacon
+				if f then
+					f(player)
+				end
+			end
 			player:SetOrigin(spawnPoints[self.i])
 			player:TriggerBeaconEffects()
 			self.i = self.i + 1
